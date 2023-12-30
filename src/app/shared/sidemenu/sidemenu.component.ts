07ee7b6fdf54be73566deda7d13b3626f7e6f10c
victor-val <vval26v@gmail.com>
2023-12-30 17:05:22 +0100

    Dashboard configuration with tailwind

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,14 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { routes } from '../../app.routes';
 
 @Component({
   selector: 'app-sidemenu',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
-  template: `<p>sidemenu works!</p>`,
+  imports: [CommonModule],
+  templateUrl: './sidemenu.component.html',
   styleUrl: './sidemenu.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SidemenuComponent { }
+export class SidemenuComponent {
+  public menuItems = routes
+    .map((route) => route.children ?? [])
+    .flat()
+    .filter((route) => route && route.path)
+    .filter((route) => !route.path?.includes(':'));
+}
