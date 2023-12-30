f22fe52b9630a791d430e101e9a10a6673fb040d
victor-val <vval26v@gmail.com>
2023-12-30 22:32:32 +0100

    ChangeDetection OnPush

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
 
+/**
+ * Sin el ChangeDetectionStrategy onPush el cambio en frameworkAsProperty lo mostraria
+ * Con OnPush se necesita indicar cuando sucede el cambio
+ * Con las signals se hace el update y se retorna nuevo objeto porque si no, no se aplicaria el cambio
+ */
 @Component({
   standalone: true,
-  imports: [],
-  templateUrl: './change-detection.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [CommonModule, TitleComponent],
+  template: `
+     <app-title [title]="currentFramework()" />
+    <pre> {{ frameworkAsSignal() | json }} </pre>
+    <pre> {{ frameworkAsProperty | json }} </pre>
+  `,
   styles: ``
 })
 export default class ChangeDetectionComponent {
 
+  public currentFramework = computed(
+    () => `Change detection - ${ this.frameworkAsSignal().name }`
+  );
+
+  public frameworkAsSignal = signal({
+    name: 'Angular',
+    releaseDate: 2016,
+  });
+
+  public frameworkAsProperty = {
+    name: 'Angular',
+    releaseDate: 2016,
+  };
+
+  constructor(){
+    setTimeout(() => {
+      // this.frameworkAsProperty.name = 'React';
+      this.frameworkAsSignal.update( value => {
+        value.name = 'React';
+        return {...value};
+      })
+      console.log('Hecho');
+    }, 3000);
+  }
 }
