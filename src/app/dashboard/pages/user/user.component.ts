d8667e834e26b492a57a0ac02ac2a6764dc6ca69
victor-val <vval26v@gmail.com>
2023-12-31 12:05:23 +0100

    Observable to signal and show user detail

--- src/app/dashboard/pages/user/user.component.ts
+++ src/app/dashboard/pages/user/user.component.ts
@@ -1,11 +1,56 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, computed, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { UsersService } from '@services/users.service';
+import { TitleComponent } from '@shared/title/title.component';
+import { switchMap } from 'rxjs';
 
 @Component({
   standalone: true,
-  imports: [],
-  templateUrl: './user.component.html',
-  styles: ``
+  imports: [CommonModule, TitleComponent],
+  template: `
+    <app-title [title]="titleLabel()" />
+
+    @if( user() ) {
+      <section>
+        <img [srcset]="user()!.avatar" [alt]="user()!.first_name" />
+
+        <div>
+          <h3>{{ user()?.first_name }} {{ user()?.last_name }}</h3>
+          <p>{{ user()?.email }}</p>
+        </div>
+      </section>
+
+    } @else {
+      <p>Cargando información</p>
+    }
+  `,
 })
 export default class UserComponent {
+  private route = inject(ActivatedRoute);
+  private usersService = inject(UsersService);
 
+  // public user = signal<User| undefined>(undefined);
+  public user = toSignal(
+    this.route.params.pipe(
+      switchMap(({ id }) => this.usersService.getUserById(id))
+    )
+  );
+
+  public titleLabel = computed(() => {
+    if (this.user()) {
+      return `Información del usuario: ${this.user()?.first_name} ${
+        this.user()?.last_name
+      } `;
+    }
+
+    return 'Información del usuario';
+  });
+
+  // constructor() {
+  //   this.route.params.subscribe(params => {
+  //     console.log({params});
+  //   })
+  // }
 }
