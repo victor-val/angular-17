e9c29c9f3ac60d91bc63ac16fc6f4e156d7a30e6
victor-val <vval26v@gmail.com>
2023-12-31 10:46:49 +0100

    Defer blocks

--- src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
+++ src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-heavy-loaders-slow',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   template: `
   <h1>Hola Mundo</h1>
+  <section [ngClass]="['w-full h-[600px]', cssClass]">
+    Heavy loader slow
+  </section>
   `,
 
 })
 export class HeavyLoadersSlowComponent {
+
+  @Input({required: true}) cssClass!: string; 
+
+  constructor(){
+    console.log("HeavyLoadersSlowComponent");
+    const start = Date.now();
+    while(Date.now() - start < 3000){ //Codigo bloqueante, tarda 3 seg en mostrar y dejar interaccionar
+      console.log("Cargado")
+    }
+  }
 
 }
