9c33a2fba8f35319656df92361d5ffff9978eb1d
victor-val <vval26v@gmail.com>
2023-12-31 11:18:17 +0100

    View transitions animated

---
+++ src/app/dashboard/pages/view-transition/view-transition2.component.ts
@@ -0,0 +1,27 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule, TitleComponent],
+  template: `
+    <app-title title="View Transition 2" />
+
+    <section class="flex justify-end">
+      <img
+        srcset="https://picsum.photos/id/237/200/300"
+        alt="Picsum"
+        width="200"
+        height="300"
+        style="view-transition-name: hero1"
+      />
+
+      <div
+        class="fixed bottom-16 rigth-10 bg-blue-800 w-32 h-32 rounded"
+        style="view-transition-name: hero2"
+      ></div>
+    </section>
+  `,
+})
+export default class ViewTransition2Component {}
